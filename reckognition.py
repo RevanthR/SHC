56c94afb7a542a3f1b9eb6d556c57f5f2a76e6c6
RevanthR <revanthrobbie37@gmail.com>
2019-05-03 14:16:19 +0530

    rekognition

---
+++ reckognition.py
@@ -0,0 +1,23 @@
+import boto3
+import re
+
+if __name__ == "__main__":
+
+    bucket='bucket1io'
+    photo='Ct-scan.png'
+    pattern='zea[0-9]'
+    client=boto3.client('rekognition')
+
+  
+    response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
+                        
+    textDetections=response['TextDetections']
+    id=[]
+    for text in textDetections:
+            if re.search(pattern,text['DetectedText'].lower()):
+                # print(type(text['DetectedText'].lower()))
+                if text['DetectedText'] not in id:
+                    id.append(text['DetectedText'])
+    
+    
+    print(id)
