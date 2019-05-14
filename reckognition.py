2eb08f4b39e353f337093e1cdf3ec5b8d0f198d8
RevanthR <revanthrobbie37@gmail.com>
2019-05-14 12:56:20 +0530

    SHC final

--- reckognition.py
+++ reckognition.py
@@ -5,19 +5,21 @@ if __name__ == "__main__":
 
     bucket='bucket1io'
     photo='scan2.png'
-    pattern='zea[0-9]'
+    pattern='ZEA[0-9]+'
     client=boto3.client('rekognition')
 
   
     response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
                         
     textDetections=response['TextDetections']
+    textDetections=str(textDetections)
     id=[]
-    for text in textDetections:
-            if re.search(pattern,text['DetectedText'].lower()):
-                # print(type(text['DetectedText'].lower()))
-                if text['DetectedText'] not in id:
-                    id.append(text['DetectedText'])
+    #for text in textDetections:
+            # if re.search(pattern,text['DetectedText'].lower()):
+            #     # print(type(text['DetectedText'].lower()))
+            #     if text['DetectedText'] not in id:
+            #         id.append(text['DetectedText'])
     
-    
-    print(id)
+    val=re.findall(pattern,textDetections)
+    val=set(val)
+    print(val)
